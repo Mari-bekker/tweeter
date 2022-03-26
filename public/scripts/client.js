d5b0622cc15398dc86f2f83e92a724178832e9af
Mari-bekker <mshooch@gmail.com>
2022-03-26 19:01:31 -0400

    fix rendertweets function

--- public/scripts/client.js
+++ public/scripts/client.js
@@ -2,30 +2,30 @@
  * Client-side JS logic
  * jQuery is already loaded
  */
-const data = [
-  {
-    "user": {
-      "name": "Newton",
-      "avatars": "https://i.imgur.com/73hZDYK.png"
-      ,
-      "handle": "@SirIsaac"
-    },
-    "content": {
-      "text": "If I have seen further it is by standing on the shoulders of giants"
-    },
-    "created_at": 1461116232227
-  },
-  {
-    "user": {
-      "name": "Descartes",
-      "avatars": "https://i.imgur.com/nlhLi3I.png",
-      "handle": "@rd" },
-    "content": {
-      "text": "Je pense , donc je suis"
-    },
-    "created_at": 1461113959088
-  }
-]
+// const data = [
+//   {
+//     "user": {
+//       "name": "Newton",
+//       "avatars": "https://i.imgur.com/73hZDYK.png"
+//       ,
+//       "handle": "@SirIsaac"
+//     },
+//     "content": {
+//       "text": "If I have seen further it is by standing on the shoulders of giants"
+//     },
+//     "created_at": 1461116232227
+//   },
+//   {
+//     "user": {
+//       "name": "Descartes",
+//       "avatars": "https://i.imgur.com/nlhLi3I.png",
+//       "handle": "@rd" },
+//     "content": {
+//       "text": "Je pense , donc je suis"
+//     },
+//     "created_at": 1461113959088
+//   }
+// ]
 
 $( document ).ready(function() {
   console.log( "ready!" );
@@ -66,7 +66,7 @@ $( document ).ready(function() {
     }
   }
 
-  renderTweets(data);
+  //renderTweets(data); test drive for the function
 
     //add an event listener for submit
     $("#postingTweet").submit(function( event ) {
@@ -84,5 +84,21 @@ $( document ).ready(function() {
         data: textdata,
         })
     });
+
+    const loadTweets = function () {
+      // fetch tweets from the http://localhost:8080/tweets page.
+      // use jQuery to make a request to /tweets and receive the array of tweets as JSON.
+      $.ajax({
+        type: 'GET',
+        url: "/tweets/",
+        dataType: 'JSON'
+      })
+      .done(function(data) {
+          renderTweets(data)
+      })
+  
+    }
+    loadTweets();  
+
 });
 
