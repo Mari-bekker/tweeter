03e21d5d038dfc6fa6c0d1d4eb2809039561e4f2
Mari-bekker <mshooch@gmail.com>
2022-03-25 22:13:35 -0400

    WIP of create tweet element

--- public/scripts/client.js
+++ public/scripts/client.js
@@ -4,3 +4,50 @@
  * Reminder: Use (and do all your DOM work in) jQuery's document ready function
  */
 
+
+const tweetData = {
+  "user": {
+    "name": "Newton",
+    "avatars": "https://i.imgur.com/73hZDYK.png",
+      "handle": "@SirIsaac"
+    },
+  "content": {
+      "text": "If I have seen further it is by standing on the shoulders of giants"
+    },
+  "created_at": 1461116232227
+}
+
+$( document ).ready(function() {
+  console.log( "ready!" );
+  const createTweetElement = function (tweetData) {
+    const $tweet = `<article>
+    <div class="tweet-header">
+      <div class="user-name"> 
+        <img class ="avatar" src="https://i.imgur.com/73hZDYK.png" alt="">
+        <h4>${tweetData.user.name}</h4>
+      </div>
+  
+      <div>${tweetData.user.handle}</div>
+  
+  
+    </div>
+    Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.
+    <footer>
+      <div>10 days ago</div>
+      <div id="icons">
+      <i class="fa-solid fa-flag"></i>
+      <i class="fa-solid fa-retweet"></i>
+      <i class="fa-solid fa-heart"></i>
+    </div>
+    </footer>
+  </article>`
+  
+  return $tweet;
+  };
+  
+  const $tweet = createTweetElement(tweetData);
+  
+  console.log($tweet); // to see what it looks like
+  $('#tweets-container').append($tweet);
+});
+
