f8c6ba01bde0cbd7723155f5395eeb444545d12c
Mari-bekker <mshooch@gmail.com>
2022-03-26 20:32:08 -0400

    remove commented debugging code

--- public/scripts/client.js
+++ public/scripts/client.js
@@ -2,31 +2,6 @@
  * Client-side JS logic
  * jQuery is already loaded
  */
-// const data = [
-//   {
-//     "user": {
-//       "name": "Newton",
-//       "avatars": "https://i.imgur.com/73hZDYK.png"
-//       ,
-//       "handle": "@SirIsaac"
-//     },
-//     "content": {
-//       "text": "If I have seen further it is by standing on the shoulders of giants"
-//     },
-//     "created_at": 1461116232227
-//   },
-//   {
-//     "user": {
-//       "name": "Descartes",
-//       "avatars": "https://i.imgur.com/nlhLi3I.png",
-//       "handle": "@rd" },
-//     "content": {
-//       "text": "Je pense , donc je suis"
-//     },
-//     "created_at": 1461113959088
-//   }
-// ]
-
 $( document ).ready(function() {
   console.log( "ready!" );
 
@@ -65,8 +40,6 @@ $( document ).ready(function() {
       $('#tweet-container').append($tweet); // can also use prepend for begining. 
     }
   }
-
-  //renderTweets(data); test drive for the function
 
     //add an event listener for submit
     $("#postingTweet").submit(function( event ) {
